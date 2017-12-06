9a63d8c54c4e6de67f4128db357e93126006d65c
Nikolay Galkin <nikolay@exaltcg.com>
2017-12-06 09:09:26 +0200

    Popup

---
+++ popup/popup.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // TODO: https://stackoverflow.com/questions/47485053/how-do-i-create-native-looking-popups-in-a-web-extension-for-firefox
+  console.info('Popup Loaded');
+});
